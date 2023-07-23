cf4a2b6dcb5f31b9a533c040932287293c4e5d1a
Philippe <philippe.brunet.494@gmail.com>
2023-07-23 14:57:24 -0400

    employee

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit{
+  public employees: Employee[] | undefined;
 
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    // this.employeeService.fetchEmployees().subscribe((response: Employee[])=>{this.employees = response},
+    //  (error : HttpErrorResponse)=> {alert("error fetching employees")}
+    //  );
+
+     this.employeeService.fetchEmployees().subscribe({
+        next: (response: Employee[])=>{this.employees = response},
+        error: (error : HttpErrorResponse)=> {alert("error fetching employees : " + error.message)}
+     });
+  }
 }
